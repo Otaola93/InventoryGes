a650d67ddac4aebd4fa1179681848716b5b66a60
Antonio Vázquez Blanco <antoniovazquezblanco@gmail.com>
2015-01-14 19:09:22 +0100

    Improve main UI.

--- com/inventoryges/InventoryGes.java
+++ com/inventoryges/InventoryGes.java
@@ -4,10 +4,11 @@ import com.inventoryges.data.Product;
 import com.inventoryges.data.providers.SerializedDataProvider;
 import com.inventoryges.data.providers.LockException;
 
+import java.awt.Dimension;
+import java.awt.GridBagLayout;
+import java.awt.GridBagConstraints;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
-
-import java.awt.Dimension;
 
 import javax.swing.JFrame;
 import javax.swing.BoxLayout;
@@ -21,8 +22,6 @@ public class InventoryGes extends JFrame implements ActionListener
 {
 	private SerializedDataProvider mStock = new SerializedDataProvider();
 
-	private JList<Product> mList;
-
 	public static void main(String args[])
 	{
 		new InventoryGes();
@@ -30,46 +29,63 @@ public class InventoryGes extends JFrame implements ActionListener
 
 	public InventoryGes()
 	{
-		this.setLayout(new BoxLayout(this.getContentPane(), BoxLayout.Y_AXIS));
-		this.setTitle("StockGes");
+		this.setTitle("InventoryGes");
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		this.setLocation(625, 420);
-		this.setSize(500, 240);
+
+		// Gridbag layout...
+		this.getContentPane().setLayout(new GridBagLayout());
+		GridBagConstraints constraints = new GridBagConstraints();
+
+		// Products list...
+		JList<Product> list = new JList<Product>(mStock);
+		list.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+		list.setLayoutOrientation(JList.VERTICAL);
+		list.setVisibleRowCount(-1);
+
+		// Inside a scroll...
+		JScrollPane pane = new JScrollPane(list, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED, JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+		pane.setPreferredSize(new Dimension(300, 200));
+
+		// Add the list to GridBag...
+		constraints.gridx = 0;	// Start on column 0
+		constraints.gridy = 0;	// Start on row 0
+		constraints.gridwidth = 2;	// 1 column width
+		constraints.gridheight = GridBagConstraints.RELATIVE;	// Height until next component
+		this.getContentPane().add(pane, constraints);
+
+		// Sync button...
+		JButton update = new JButton("Update");
+		update.addActionListener(this);
+		this.add(update);
+
+		// Add the sync button...
+		constraints.gridx = 0;	// Start on column 0
+		constraints.gridy = 1;	// Start on row 1
+		constraints.gridwidth = 1;	// 1 column width
+		constraints.gridheight = 1;	// 1 row height
+		this.getContentPane().add(update, constraints);
+
+		// Add button...
+		JButton add = new JButton("Add");
+		add.addActionListener(this);
+		this.add(add);
+
+		// Add the sync button...
+		constraints.gridx = 1;	// Start on column 1
+		constraints.gridy = 1;	// Start on row 1
+		constraints.gridwidth = 1;	// 1 column width
+		constraints.gridheight = 1;	// 1 row height
+		this.getContentPane().add(add, constraints);
+
+		// Show window...
+		this.pack();
 		this.setVisible(true);
-
-		mList = new JList<Product>(mStock);
-		mList.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
-		mList.setLayoutOrientation(JList.VERTICAL);
-		mList.setVisibleRowCount(-1);
-		this.add(mList);
-
-		JButton button = new JButton("Lock");
-		button.addActionListener(this);
-		this.add(button);
-
-		JButton button2 = new JButton("Unlock");
-		button2.addActionListener(this);
-		this.add(button2);
-
-		JButton button3 = new JButton("Add");
-		button3.addActionListener(this);
-		this.add(button3);
-
-		JButton button4 = new JButton("Update");
-		button4.addActionListener(this);
-		this.add(button4);
 	}
 
 	public void actionPerformed(ActionEvent e)
 	{
 		switch(e.getActionCommand())
 		{
-		case "Lock":
-			mStock.getLock();
-			break;
-		case "Unlock":
-			mStock.releaseLock();
-			break;
 		case "Add":
 			try
 			{
